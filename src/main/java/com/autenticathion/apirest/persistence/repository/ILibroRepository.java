2a1bcf1f4e453f85e5ee8c49f4fd585e34232c85
Sara Cardona <saravalcrdn@gmail.com>
2023-10-11 10:25:21 -0500

    feat basic impl

--- src/main/java/com/autenticathion/apirest/persistence/repository/ILibroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.autenticathion.apirest.persistence.repository;
-
-import com.autenticathion.apirest.persistence.models.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-/*public interface ILibroRepository extends JpaRepository<Libro, Long> {
-
-}*/
